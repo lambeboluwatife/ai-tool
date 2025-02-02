0fdb51bd817e732c44ec04df6412cad5d074b8d9
lambeboluwatife <boluwatifelambe@gmail.com>
2025-02-02 06:54:28 +0100

    some result update

--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,13 @@ export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat({
     maxSteps: 10,
   });
+  console.log(messages);
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
       {messages.map((m) => (
         <div key={m.id} className="whitespace-pre-wrap">
           {m.role === "user" ? "User: " : "AI: "}
-          {m.toolInvocations ? (
-            // Since this might be JSON data, just display the result from the tool call instead
-            <p>{m.toolInvocations[0]?.result}</p>
-          ) : (
-            // Otherwise, just display the normal content (which will be the AI's response)
-            <p>{m.content}</p>
-          )}
+          {m.toolInvocations ? <p>{m.content}</p> : <p>{m.content}</p>}
         </div>
       ))}
 
